52860d3facf136ff57f5acb111156d3444ad7295
jobstoit <jobstoit@icloud.com>
2023-02-13 01:46:56 +0100

    fix typo

--- rust.sh
+++ rust.sh
@@ -5,7 +5,7 @@
  export PATH="~/.cargo/bin:$PATH"
  curl -fsSL https://sh.rustup.rs | sh -s -- -y --profile minimal --no-modify-path --default-toolchain stable \
  		-c rls rust-analysis rust-src rustfmt clippy && \
-	echo "export PATH=~/.cargo/bin:\$PATH\nsource <(rustup completions \$(echo \$SHELL | sed -e 's#^\(/\w+\)*/##g') rustup)\nsource <(rustup completions \$(echo \$SHELL | sed -e 's#^\(/\w+\)*/##g') cargo)" > ~/.config/profile.d/_cargo && \
+	echo "export PATH=~/.cargo/bin:\$PATH\nsource <(rustup completions \$(echo \$SHELL | sed -e 's#^\(/\w*\)*/##g') rustup)\nsource <(rustup completions \$(echo \$SHELL | sed -e 's#^\(/\w*\)*/##g') cargo)" > ~/.config/profile.d/_rust && \
  	rustup component add rust-analyzer && \
  	cargo version
 
