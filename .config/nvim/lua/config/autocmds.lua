0f1ba4bd7220c66a60768fdca9534cdabea598e9
Job Stoit <job@jobstoit.me>
2024-03-02 14:38:37 +0100

    chore: configure lazyvim for neovim

---
+++ .config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,11 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+vim.cmd([[
+
+hi! Normal ctermbg=NONE guibg=NONE
+hi! NormalNC ctermbg=NONE guibg=NONE
+hi! LineNr ctermbg=NONE guibg=NONE
+hi! NonText ctermbg=NONE guibg=NONE
+
+]])
---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "hrsh7th/nvim-cmp", enabled = false },
+}
