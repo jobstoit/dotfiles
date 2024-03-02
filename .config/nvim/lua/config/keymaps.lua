32f86f774a1b289d5ef6c8128d60a115bf68abe6
Job Stoit <job@jobstoit.me>
2024-03-02 17:45:21 +0100

    feat: add additional keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "<C-n>", "<C-x><C-o>", { desc = "omnicomplete on ctrl-n", remap = true })
+vim.keymap.set("n", "<C-q>", ":q<CR>", { remap = true })
+
+vim.keymap.set("n", "gh", "0", { desc = "go to the beginning of the line" })
+vim.keymap.set("n", "gl", "$", { desc = "go to the end of the line" })
